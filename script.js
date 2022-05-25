c83124dea737d0779725d5512fc0ce262e3f4926
Mikkel <mikkellau98@hotmail.com>
2022-05-25 16:16:25 +0200

    kontakt os alert i js tilføjet

---
+++ script.js
@@ -0,0 +1,12 @@
+//kode fra https://stackoverflow.com/questions/9334636/how-to-create-a-dialog-with-ok-and-cancel-options d.25-05-2022 
+
+function sendFunction() {
+if (confirm('Er du sikker på at du vil sende denne besked?')) {
+    alert('Tak for din besked, - du modtager snarest et svar!')
+    console.log('Besked sendt til e-mail');
+} else {
+    console.log('Besked ikke afsendt');
+}
+  }
+
+
