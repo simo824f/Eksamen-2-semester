582362f94ee142e6e55b72d733004b1c73f11dfa
Heinrich wolff <hcwolff94@gmail.com>
2022-06-08 21:29:35 +0200

    nyt slide og små rettelser

--- script.js
+++ script.js
@@ -8,36 +8,3 @@ if (confirm('Er du sikker på at du vil sende denne besked?')) {
     console.log('Besked ikke afsendt');
 }
   }
-
-
-
-//forsiden
-  let slideIndex = 1;
-  slideshowBeholder(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    slideshowBeholder(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    slideshowBeholder(slideIndex = n);
-}
-
-function slideshowBeholder(n) {
-  let i;
-  let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
-
